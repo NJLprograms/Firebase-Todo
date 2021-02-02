cc2f1dbbcfaf56689082747617bd57dde48dc75c
Niels Louis <neils_justin@hotmail.com>
2021-02-02 03:40:27 -0500

    remove unused vars

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,25 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css';
-import React, {useState} from 'react';
-import { Router, navigate } from '@reach/router';
+import React from 'react';
+import { Router } from '@reach/router';
 import Navigation from './Navigation';
-import Home from './Home'
+import Home from './Home';
 import Login from './Login';
 import Todo from './Todo';
 import Register from './Register';
 
 const App = () => {
-  const [user, editUser] = useState({displayName:"Bob", userID:1});
   return (
-  <div>
-    <Navigation user={null}/>
-    <Router>
-      <Home path='/' user={null}/>
-      <Login path='/login'/>
-      <Todo path='/todo'/>
-      <Register path='/register'/>
-    </Router>
-  </div>
+    <div>
+      <Navigation user={null} />
+      <Router>
+        <Home path="/" user={null} />
+        <Login path="/login" />
+        <Todo path="/todo" />
+        <Register path="/register" />
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,10 @@
-import {Button, Form, Nav} from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { useState } from 'react';
 import FormError from './FormError';
-import {auth} from '../utils/Firebase';
-import {navigate} from "@reach/router" 
+import { auth } from '../utils/Firebase';
+import { navigate } from '@reach/router';
 
 const Login = () => {
-
   const [email, setEmail] = useState('');
   const [error, setError] = useState(null);
   const [password, setPassword] = useState('');
@@ -14,55 +13,54 @@ const Login = () => {
     event.preventDefault(); // prevent page from refreshing on submit
 
     console.log(email, password);
-    auth.signInWithEmailAndPassword(
-        email,
-        password
-    ).catch(error=>{
-        if(error.messsage !== null){
-          setError(error.message)
-        }else {
-          setError(null)
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        if (error.messsage !== null) {
+          setError(error.message);
+        } else {
+          setError(null);
         }
-    }).then(() => {
+      })
+      .then(() => {
         navigate('/');
-    })
+      });
   };
 
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <Form className="mt-3" onSubmit={handleSubmit}>
+        {error && <FormError message={error} />}
 
-    return(
-        <div style={{display:'flex', justifyContent:"center"}}>
-            <Form className="mt-3" onSubmit={handleSubmit} > 
-            {error &&( <FormError message={error}/>)}
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
 
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control 
-                        type="email" 
-                        placeholder="Enter email"
-                        onChange={(event) => {
-                            setEmail(event.target.value);
-                        }} 
-                    />
-                    <Form.Text className="text-muted"></Form.Text>
-                </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control 
-                        type="password" 
-                        placeholder="Password"
-                        onChange={(event) => {
-                            setPassword(event.target.value);
-                        }} 
-                    />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                     Login
-                </Button>
-            </Form> 
-        </div>
-    )
-}
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default Login;
